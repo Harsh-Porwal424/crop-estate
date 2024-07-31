1d867bb276930587656edd415a36afcb09fd5383
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-07-31 14:37:47 +0530

    Connected with frontend register

--- api/app.js
+++ api/app.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import postRoute from './routes/post.routes.js'; // Ensure you have the correct file extension
 import authRoute from './routes/auth.routes.js'; // Ensure you have the correct
 
 const app = express();
 
+app.use(cors({origin: process.env.CLIENT_URL, credentials:true})); // for handling cross-origin requests
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser()); // for parsing cookies
 
