27a0c738f75348f895c30278b53f809a855c4bf0
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-07-31 12:55:41 +0530

    Added User register & login

--- api/app.js
+++ api/app.js
@@ -1,9 +1,17 @@
+import 'dotenv/config';
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import postRoute from './routes/post.routes.js'; // Ensure you have the correct file extension
+import authRoute from './routes/auth.routes.js'; // Ensure you have the correct
 
 const app = express();
 
+app.use(express.json()); // for parsing application/json
+app.use(cookieParser()); // for parsing cookies
+
 app.use("/api/post", postRoute);
+app.use("/api/auth", authRoute);
+
 
 app.listen(8800, () => {
     console.log('Server is running on port 8800');
---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,74 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import prisma from "../lib/prisma.js";
+
+export const register = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user in the database
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        // Send a response back to the client
+        res.status(201).json({ message: "User registered successfully"});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "An error occurred while registering the user" });
+    }
+};
+
+export const login = async (req, res) => {
+    // Implement login logic
+
+    const { username, password } = req.body;
+    try{
+        //check if the user exists in the database
+
+        const user = await prisma.user.findUnique({
+            where: {
+                username,
+            },
+        });
+
+        if(!user){
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        //check user passworkd is correct
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        
+        //genearte JWT token and send it back to the client
+        // res.setHeader("Set-Cookie", "test="+"myValue").json("sucessful login");
+        const age = 1000 * 60 * 60 * 24 * 7;
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: age }); 
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            maxAge: age,
+            // secure: true
+        }).status(200).json({ message: "User logged in successfully" });
+
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({ message: "An error occurred while logging in the user" });
+    }
+
+};
+
+export const logout = (req, res) => {
+    // Implement logout logic
+};
