e02db91a455caa9755b8ea862a3dea725559451c
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-07-31 15:16:25 +0530

    testing commit

---
+++ api/fdsg.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import postRoute from './routes/post.routes.js'; // Ensure you have the correct file extension
+import authRoute from './routes/auth.routes.js'; // Ensure you have the correct
+
+const app = express();
+
+app.use(cors({origin: process.env.CLIENT_URL, credentials:true})); // for handling cross-origin requests
+app.use(express.json()); // for parsing application/json
+app.use(cookieParser()); // for parsing cookies
+
+app.use("/api/post", postRoute);
+app.use("/api/auth", authRoute);
+
+
+app.listen(8800, () => {
+    console.log('Server is running on port 8800');
+});
